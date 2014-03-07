0dab24fb316a55b2d28eb4c75f87b46d6cc52ebe
Chad <Chad@Red.ten.thomsonreuters.com>
2014-03-06 21:08:40 -0600

    CNogosek: Cleaned some noise on ViewController and added JavaDoc.

--- Poker/src/com/nogo/poker/ViewController.java
+++ Poker/src/com/nogo/poker/ViewController.java
@@ -1,33 +1,39 @@
 package com.nogo.poker;
 
-import java.util.Locale;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
- * Servers up the web pages.
+ * Serves up web pages.
  */
 @Controller
 public class ViewController {
 
+	/**
+	 * Serves up the default page
+	 *
+	 * @return home.html
+	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
-	public String home(Locale locale, Model model, HttpServletRequest request) {
+	public String home() {
 		return "home";
 	}
 
+	/**
+	 * Serves requested web page and robustly handles variations of home page requests.
+	 *
+	 * @param page
+	 *            requested web page
+	 * @return {page}.html
+	 */
 	@RequestMapping(value = "/{page}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
-	public String page(Locale locale, Model model,
-			@PathVariable(value = "page") String page) {
+	public String page(@PathVariable(value = "page") String page) {
 		if ("home".equals(page) | "home.html".equals(page)
 				| "home.jsp".equals(page)) {
 			return "home";
